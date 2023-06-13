a7843bee4ce07b66b2f3e6858c76f9e28ff9d5e2
kuldeepsingh0330 <kuldeepsingh41424344@gmail.com>
2023-06-13 17:36:54 +0530

    project created

---
+++ src/main/java/com/ransankul/clickmart/ClickmartApplication.java
@@ -0,0 +1,13 @@
+package com.ransankul.clickmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickmartApplication.class, args);
+	}
+
+}
